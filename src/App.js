413346b4cc64b12046c46bd12571a7fdbf672134
EraNova94 <103873419+EraNova94@users.noreply.github.com>
2022-10-23 15:18:50 +0600

    first-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import InpList from "./Components/InpList/InpList";
+import Navbar from "./Components/Navbar/Navbar";
+import AddContact from "./Components/AddContact/AddContact";
+import EditContacts from "./Components/EditContacts/EditContacts";
 
-function App() {
+const App = () => {
+  const [listArr, setListArr] = useState([]);
+  const [show, setShow] = useState(false);
+  const [singleEditContact, setSingleEditContact] = useState({});
+
+  function addContacts(objContact) {
+    let newListArr = [...listArr];
+    newListArr.push(objContact);
+    setListArr(newListArr);
+  }
+  function contactDelete(id) {
+    let newContactArr = listArr.filter(item => {
+      return item.id !== id;
+    });
+    setListArr(newContactArr);
+  }
+
+  function saveEditedContact(editedContactObj) {
+    console.log(editedContactObj);
+    let newEditedContact = listArr.map(item => {
+      console.log(item);
+      if (item.id === editedContactObj.id) {
+        return editedContactObj;
+      } else {
+        return item;
+      }
+    });
+    setListArr(newEditedContact);
+    console.log(newEditedContact);
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("list") === null) {
+      localStorage.setItem("list", JSON.stringify([]));
+    } else {
+      let data = localStorage.getItem("list");
+      // console.log(data);
+      setListArr(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(listArr));
+  }, [listArr]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <InpList addContacts={addContacts} />
+      <AddContact
+        listArr={listArr}
+        contactDelete={contactDelete}
+        setSingleEditContact={setSingleEditContact}
+        setShow={setShow}
+      />
+      <EditContacts
+        saveEditedContact={saveEditedContact}
+        singleEditContact={singleEditContact}
+        setShow={setShow}
+        show={show}
+      />
     </div>
   );
-}
+};
 
 export default App;
